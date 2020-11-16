5d7af7161bb863e1e7fb993949af09e5ab5fcc28
singh-rbir <43500177+singh-rbir@users.noreply.github.com>
2020-11-16 17:37:38 -0500

    rename controller package

--- src/main/java/com/project/bookstore/controller/MainController.java
+++ src/main/java/com/project/bookstore/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.project.bookstore.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
