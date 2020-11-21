b2abce53524052a4b1de93181a99a13ce566b19c
singh-rbir <43500177+singh-rbir@users.noreply.github.com>
2020-11-21 17:16:22 -0500

    add request mapping for book controller

--- src/main/java/com/project/bookstore/controller/BookController.java
+++ src/main/java/com/project/bookstore/controller/BookController.java
@@ -4,12 +4,14 @@ import com.project.bookstore.model.BookEntity;
 import com.project.bookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/book")
 public class BookController {
 
     @Autowired
