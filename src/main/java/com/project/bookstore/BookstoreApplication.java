2c52927571331bbde336bf604418ef2687eadaa6
singh-rbir <43500177+singh-rbir@users.noreply.github.com>
2020-12-05 16:09:00 -0500

    Convert project to support eclipse runnable
    + cleanup imports

--- src/main/java/com/project/bookstore/BookstoreApplication.java
+++ src/main/java/com/project/bookstore/BookstoreApplication.java
@@ -1,21 +1,13 @@
 package com.project.bookstore;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
-import java.util.Properties;
 
 //@EnableJpaRepositories(basePackages = "com.project.bookstore.dao")
 @SpringBootApplication
@@ -24,7 +16,7 @@ public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
+	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
